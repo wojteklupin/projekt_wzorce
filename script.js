9afd1b7cf74e7739407caf6b44e6831cff267037
wojteklupin <wojteklupin@gmail.com>
2019-01-07 16:19:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -48,7 +48,28 @@ const server = http.createServer((req, res) => {
                     });
                 }
             }
+            else if(typeof form.rach_odb !== 'undefined') { // jesli to byla strona przelewu
+                var sql = `SELECT pieniadze FROM klienci WHERE nr_rachunku = `+form.rach;
+                con.query(sql, function(err,result) {
+                    if(err) throw err;
+                    else if(result[0].pieniadze < form.kwota) {
+                        res.end("Zbyt mało pieniędzy na koncie");
+                    }
+                    else {
+                        sql  = `UPDATE klienci SET pieniadze = pieniadze-${form.kwota} WHERE nr_rachunku = ${form.rach};`;
+                        sql2 = `UPDATE klienci SET pieniadze = pieniadze+${form.kwota} WHERE nr_rachunku = ${form.rach_odb};`;
+                        con.query(sql, function(err, result) {
+                            if(err) res.end("Nie ma konta o takim nr rachunku");
+                        });
+                        con.query(sql2, function(err, result) {
+                            if(err) throw err;
+                            res.end("Przelane!");
+                        });
+                    }
+                });
+            }
             else { // jesli to byla strona logowania
+                nr_rach = form.rach;
                 var sql = `SELECT haslo, bank FROM klienci WHERE nr_rachunku = `+form.rach;
                 con.query(sql, function (err, result) {
                     if(err) throw err;
@@ -58,7 +79,30 @@ const server = http.createServer((req, res) => {
                         sql = `SELECT pieniadze FROM klienci WHERE nr_rachunku = `+form.rach;
                         con.query(sql, function (err, result) {
                             if(err) throw err;
-                            res.end("Stan konta to "+result[0].pieniadze);
+                            res.end(`<html>
+                                       <head>
+                                         <meta charset="UTF-8">
+                                       </head>
+                                       
+                                       <body>
+                                         Stan konta to ${result[0].pieniadze} złotych<br>
+                                         Zrób przelew:<br>
+                                         <form action="http://localhost:8080/" method="post">
+                                           <table>
+                                           <tr>
+                                             <td>Twój nr rachunku:</td><td><input type="text" name="rach" readonly value=${form.rach}></td><td></td>
+                                           </tr>
+                                           <tr>
+                                             <td>Nr rachunku odbiorcy:</td><td><input type="text" name="rach_odb"></td><td></td>
+                                           </tr>
+                                           <tr>
+                                             <td>Kwota:</td><td><input type="text" name="kwota"></td><td><input type="submit" value="Przelej"></td>
+                                           </tr>
+                                           </table>
+                                         </form>
+                                       </body>
+                                     </html>
+                            `);
                         });
                     }
                 });
